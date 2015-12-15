97b1e375f35aafe0dc24c1e82e6ecf99fd686611
zhengqiangzi <zhengqiangzi123@163.com>
2015-12-15 15:02:27 +0800

    'autosendmessage'

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path=require('path');
+var current={
+	team1:0,
+	team2:0,
+	team3:0,
+	team4:0,
+	team5:0,
+	max:10000
+}
 app.use('/img',express.static(path.join(__dirname,'img')));
 app.use('/dist',express.static(path.join(__dirname,'dist')));
 app.get('/', function(req, res){
@@ -16,14 +24,19 @@ http.listen(3000, function(){
 });
 
 io.on('connection',function(socket){
+
+	io.emit('scoreMessage',current)
 	//接收积分
 	socket.on('changeMessage',function(socket){
+		
+
 		socket.team1=parseInt(socket.team1)||0;
 		socket.team2=parseInt(socket.team2)||0;
 		socket.team3=parseInt(socket.team3)||0;
 		socket.team4=parseInt(socket.team4)||0;
 		socket.team5=parseInt(socket.team5)||0;
 		socket.max=parseInt(socket.max)||10000;//默认最高值为10000
+		current=socket;
 		io.emit('scoreMessage',socket);
 	})
 })
